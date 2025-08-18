840eb7dc12511b672f13038bb0e8ec55fe460bd8
iyman <i49459019@gmail.com>
2025-08-18 12:10:21 +0600

    fix

--- wp-content/themes/povarenok-theme/functions.php
+++ wp-content/themes/povarenok-theme/functions.php
@@ -1,1 +1,7 @@
-
+<?php
+    // Enqueue styles and scripts
+    function povarenok_enqueue_scripts() {
+        wp_enqueue_style('povarenok-style', get_stylesheet_uri());
+        wp_enqueue_script('povarenok-script', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true);
+    }
+    add_action('wp_enqueue_scripts', 'povarenok_enqueue_scripts');
